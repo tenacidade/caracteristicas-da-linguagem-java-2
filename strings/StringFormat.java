81182626cb1b156ad3f6a8000a59f59e1179fc88
tenacidade <grd1996@gmail.com>
2021-06-04 12:42:11 -0300

    comenta utilização de strings

--- strings/StringFormat.java
+++ strings/StringFormat.java
@@ -4,17 +4,20 @@ public class StringFormat {
 
   public static void main(String[] args) {
 
-    var nome = "André";
-    var sobreNome = "Gomes";
+    var nome = "Gabriel";
+    var sobreNome = "Duarte";
     final var nomeCompleto = nome + sobreNome;
 
     System.out.println(nome);
     System.out.println("Nome do cliente : " + nome);
     System.out.println("Nome completo do cliente : " + nomeCompleto);
+
     final var mensagem = String.format("O cliente %s possui sobre nome %s ", nome, sobreNome);
+    //insere variavel nome dentro do primeiro %s e variavel sobreNome dentro do segundo %s
     System.out.println(mensagem);
 
     System.out.println(String.format("Numero %.2f ", 1.2375d));
+    //formata o numero para mostrar somente duas casas após a vírgula ... arrendonda pra 1,24.
   }
 
 }
--- strings/Strings.java
+++ strings/Strings.java
@@ -4,9 +4,9 @@ public class Strings {
 
   public static void main(String[] args) {
 
-    var nome = "André";
-    var sobreNome = "Gomes";
-    final var nomeCompleto = nome + sobreNome; //
+    var nome = "Gabriel";
+    var sobreNome = "Duarte";
+    final var nomeCompleto = nome + sobreNome;
 
     System.out.println(nome);
     System.out.println("Nome do cliente : " + nome);
@@ -15,15 +15,15 @@ public class Strings {
 
     System.out.println("Char na posição : " + string.charAt(5));
     System.out.println("Quantidade=" + string.length());
-    System.out.println("Sem Trim [" + string + "]");
-    System.out.println("Com Trim [" + string.trim() + "]");
+    System.out.println("Sem Trim [" + string + "]"); // separado do texto
+    System.out.println("Com Trim [" + string.trim() + "]"); // colado no texto
     System.out.println("Lower " + string.toLowerCase());
     System.out.println("Upper " + string.toUpperCase());
     System.out.println("Contém M? " + string.contains("M"));
     System.out.println("Contém X? " + string.contains("X"));
     System.out.println("Replace " + string.replace("n", "$"));
-    System.out.println("Equals? " + string.equals(" Minha String "));
-    System.out.println("EqualsIgnoreCase? " + string.equalsIgnoreCase(" minha sTrinG "));
+    System.out.println("Equals? " + string.equals(" Minha String ")); //false pois tem um espaço a menos.
+    System.out.println("EqualsIgnoreCase? " + string.equalsIgnoreCase(" minha sTrinG ")); //false pois tem um espaço a menos.
     System.out.println("Substring(1,6)=" + string.substring(1, 6));
 
   }
