f303927db7519cd97c789491bd2f0a4b4f9d85d0
tenacidade <grd1996@gmail.com>
2021-06-04 12:49:18 -0300

    comenta utilização de strings #2

--- strings/StringBuilder.java
+++ strings/StringBuilder.java
@@ -4,17 +4,17 @@ public class StringBuilder {
 
   public static void main(String[] args) {
 
-    var nome = "André";
+    var nome = "Gabriel";
 
     final var builder = new java.lang.StringBuilder(nome);
-    System.out.println(builder.append("Luis"));
+    System.out.println(builder.append("Duarte")); //adiciona string "Duarte" à variável nome
 
-    final var reverse = builder.reverse();
+    final var reverse = builder.reverse(); //cria uma nova string de trás pra frente
 
     System.out.println(reverse);
 
     final var insert = reverse.insert(0, "#").insert(reverse.length(), "#");
-    System.out.println(insert);
+    System.out.println(insert); //insere caracter "#" na posição 0 e na posição
 
   }
 
