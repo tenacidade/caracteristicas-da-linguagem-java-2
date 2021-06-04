e59a83649738cba9f8dff9cfaa49a11068c4ceab
tenacidade <grd1996@gmail.com>
2021-06-04 11:25:02 -0300

    inicia parte 2 caracteristisca da linguagem java

---
+++ CaracteristicasDaLinguagemJava2Application.java
@@ -0,0 +1,13 @@
+package br.unisinos.caracteristicasdalinguagemjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaracteristicasDaLinguagemJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaracteristicasDaLinguagemJava2Application.class, args);
+	}
+
+}
---
+++ operadores/Incremento.java
@@ -0,0 +1,16 @@
+package br.unisinos.caracteristicasdalinguagemjava2.operadores;
+
+public class Incremento {
+
+  public static void main(String[] args) {
+
+    var numero = 1;
+
+    System.out.println(++numero);
+
+    var variavel = 10;
+
+    System.out.println(variavel--);
+    System.out.println(variavel);
+  }
+}
